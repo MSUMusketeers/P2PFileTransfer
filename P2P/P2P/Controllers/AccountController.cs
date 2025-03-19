63a81ad99ae1050efaf676f781b64a0000362eeb
VyomShah28 <vyom509shah@gmail.com>
2025-03-20 00:18:51 +0530

    Login and Signup Pages Added

---
+++ P2P/P2P/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2P.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Email, string Password)
+        {
+            if (Email == "admin@example.com" && Password == "password")
+            {
+                return RedirectToAction("Index", "Home"); // Redirect after login
+            }
+            ViewBag.Message = "Invalid email or password";
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(string FullName, string Email, string Password)
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
--- P2P/P2P/obj/Release/net7.0/P2P.AssemblyInfo.cs
+++ P2P/P2P/obj/Release/net7.0/P2P.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2P")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4618e077dad3a8cea718269955238bf229214105")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2P")]
