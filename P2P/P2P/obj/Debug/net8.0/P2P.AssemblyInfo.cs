059aead3fca2bd7385fe6569076eb5c43fea8092
Scripter2104 <yashvsolanki81@gmail.com>
2025-04-29 10:45:20 +0530

    Added Gauge Chart

--- P2P/P2P/obj/Debug/net8.0/P2P.AssemblyInfo.cs
+++ P2P/P2P/obj/Debug/net8.0/P2P.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2P")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e8096e3c236edf522c739eecb6684b707509d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca51a98b947620b0381f9c256e6bcb0a680b56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2P")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2P")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
